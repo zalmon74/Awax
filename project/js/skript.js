7f30ebbe5a6478c182c8a12de178cb72fe96c8f1
zalmon74 <zalmon74@yandex.ru>
2022-01-26 14:40:47 +0700

    add intro

--- project/js/skript.js
+++ project/js/skript.js
@@ -2,4 +2,10 @@ $(document).ready(function(){
   $("#header__menu_toggle").click(function(event){
     $("#header__nav").toggleClass("active");
   });
+  // Intro slider
+  $(".intro__slider").slick({
+    arrows: false,
+    dots: true,
+    autoplay: true
+  });
 });
