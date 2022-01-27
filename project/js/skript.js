3834192463b35a301b9484e2d27f586f71e43e89
zalmon74 <zalmon74@yandex.ru>
2022-01-27 14:42:38 +0700

    add about + add skript tap (github)

--- project/js/skript.js
+++ project/js/skript.js
@@ -8,4 +8,54 @@ $(document).ready(function(){
     dots: true,
     autoplay: true
   });
+  // PlayStopVideo
+  $("#about__video").on("tap", function(event) {PlayStopVideo()});
+  // About Read More
+  $(".about__button").click(function(event){
+    let more_text = document.getElementById("about__more_text");
+    let button = document.getElementById("about__button");  
+    if (more_text.style.display == "none") {
+      button.value = "hidden";
+      more_text.style.display = "inline";
+    } else {
+      button.value = "learn more";
+      more_text.style.display = "none";
+    }
+  });
 });
+
+// Play Stop Video
+function PlayStopVideo() {
+  if ($(window).width() <= "1000")
+    PlayStopVideoForMobile();
+  else
+    PlayStopVideoForPC();
+}
+
+function PlayStopVideoForMobile() {
+  let video_item = document.getElementById("about__video")
+  let str_pause_video = "video_play";
+  if (video_item.classList.contains(str_pause_video))
+  {
+    video_item.pause();
+    video_item.classList.remove(str_pause_video);
+  }
+  else {
+    video_item.play();
+    video_item.classList.add(str_pause_video);
+  }
+}
+
+function PlayStopVideoForPC() {
+  let video_item = document.getElementById("about__video")
+  let str_pause_video = "video_play";
+  if (video_item.classList.contains(str_pause_video))
+  {
+    console.log("video_item.classList")
+    video_item.pause();
+    video_item.classList.remove(str_pause_video);
+  }
+  else {
+    video_item.classList.add(str_pause_video);
+  }
+}
